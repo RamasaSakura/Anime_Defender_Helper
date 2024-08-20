f95262c4bfb1fcb4ece901a1604b5a49da66e1e7
RamasaSakura <88618242+RamasaSakura@users.noreply.github.com>
2024-08-20 12:34:27 +0700

    Update Assets_Analyzer.lua

--- Extra_Feature/Assets_Analyzer.lua
+++ Extra_Feature/Assets_Analyzer.lua
@@ -1,17 +1,17 @@
 local TabLevel = 0
 local plr = game:GetService("Players").LocalPlayer
 local function PrintTable(Table)
-    for Key,Value in pairs(Table) do
-        if typeof(Value) == "table" then
-	        TabLevel = TabLevel + 1
-	        print(string.rep("    ",TabLevel - 1)..Key.." : {")
-            PrintTable(Value)
-            warn(string.rep("    ",TabLevel - 1).."}")
-            TabLevel = TabLevel - 1
-        else
-            warn(string.rep("    ",TabLevel)..Key,Value)
-        end
-    end
+	for Key,Value in pairs(Table) do
+		if typeof(Value) == "table" then
+			TabLevel = TabLevel + 1
+			warn(string.rep("    ",TabLevel - 1)..Key.." : {")
+			PrintTable(Value)
+			warn(string.rep("    ",TabLevel - 1).."}")
+			TabLevel = TabLevel - 1
+		else
+			warn(string.rep("    ",TabLevel)..Key,Value)
+		end
+	end
 end
 
 PrintTable(plr.PlayerGui.PAGES:GetChildren())
