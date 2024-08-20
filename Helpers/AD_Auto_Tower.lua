921351703ddbf9d09fbb37afedbb00903d6fc814
RamasaSakura <88618242+RamasaSakura@users.noreply.github.com>
2024-08-20 19:19:52 +0700

    Update AD_Auto_Tower.lua

--- Helpers/AD_Auto_Tower.lua
+++ Helpers/AD_Auto_Tower.lua
@@ -44,7 +44,7 @@ local PAGES_FRAMES = {
 
 local function click_this_gui(to_click: GuiObject)
 	local Inset = GuiService:GetGuiInset()
-	
+
 
 	local AbsoluteSize = to_click.AbsoluteSize
 	local Offset = {
@@ -53,80 +53,58 @@ local function click_this_gui(to_click: GuiObject)
 	}
 
 	local x,y = to_click.AbsolutePosition.X + Offset.x ,to_click.AbsolutePosition.Y + Offset.y
-	
+
 	VirtualInputManager:SendMouseButtonEvent(x+Inset.X,y+Inset.Y,0,true,game,0)
 	VirtualInputManager:SendMouseButtonEvent(x+Inset.X,y+Inset.Y,0,false,game,0)
 end
 
 local function OnReachedDestination()
 	local TowerOfEternity_Button = PAGES_FRAMES.TowerOfEternity.PlayButton :: TextButton
-	
-	
+
+
 	VirtualInputManager:SendKeyEvent(true,Enum.KeyCode.E,false,game)
-	
+
 	if not PAGES_FRAMES.TowerOfEternity.Visible then
 		PAGES_FRAMES.TowerOfEternity:GetPropertyChangedSignal("Visible"):Wait()
 	end
-	
-	task.wait(0.45)
-	
+
+	task.wait(1)
+
 	click_this_gui(TowerOfEternity_Button)
-	
-	
+
+
 	local old_pos = plr.Character:GetPivot().Position
-	
+
 	repeat task.wait() until (plr.Character:GetPivot().Position - old_pos).Magnitude >= 5 --Wait until player teleport (May wait for server)
-	
-	task.wait(0.25)
+
+	task.wait(1)
 	click_this_gui(StartButtonHolder)
-	
-	
+
+
 end
 
-local function followPath(destination)
-	-- Compute the path
-
-	local success, errorMessage = pcall(function()
-		path:ComputeAsync(character.PrimaryPart.Position, destination)
-	end)
-
-	if success and path.Status == Enum.PathStatus.Success then
-		-- Get the path waypoints
-		waypoints = path:GetWaypoints()
-
-		-- Detect if path becomes blocked
-		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
-			-- Check if the obstacle is further down the path
-			if blockedWaypointIndex >= nextWaypointIndex then
-				-- Stop detecting path blockage until path is re-computed
-				blockedConnection:Disconnect()
-				-- Call function to re-compute new path
-				followPath(destination)
-			end
-		end)
-
-		-- Detect when movement to next waypoint is complete
-		if not reachedConnection then
-			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
-				if reached and nextWaypointIndex < #waypoints then
-					-- Increase waypoint index and move to next waypoint
-					nextWaypointIndex += 1
-					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-				else
-					reachedConnection:Disconnect()
-					blockedConnection:Disconnect()
-					
-					OnReachedDestination()
-				end
-			end)
+local function followPath(destination: Vector3)
+	local TravelTime = 4
+	
+	local TimeSpent = 0
+	
+	local CN
+	
+	CN = RunService.PostSimulation:Connect(function(dt)
+		
+		TimeSpent += dt
+		
+		local percent = math.min(TimeSpent/TravelTime,1)
+		
+		plr.Character:PivotTo(CFrame.new(plr.Character:GetPivot().Position:Lerp(destination,percent)))
+		
+		if percent >= 1 then
+			CN:Disconnect()
+			task.wait(1)
+			OnReachedDestination()
 		end
-
-		-- Initially move to second waypoint (first waypoint is path start; skip it)
-		nextWaypointIndex = 2
-		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-	else
-		warn("Path not computed!", errorMessage)
-	end
+		
+	end)
 end
 
 
