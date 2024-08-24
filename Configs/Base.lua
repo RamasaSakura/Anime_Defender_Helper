09a1fd6948359639449bcd99dafbfd96bceea808
RamasaSakura <88618242+RamasaSakura@users.noreply.github.com>
2024-08-24 20:57:48 +0700

    Update Base.lua

--- Configs/Base.lua
+++ Configs/Base.lua
@@ -1,3 +1,3 @@
 return {
-  ["Enable Network Call] = true --ถ้าจะเทรดเพชรให้ตั้งเป็น true ถ้าไม่คือ false (ตั้ง false หากเซิฟล่ม)
+  ["Enable Network Call] = false --ถ้าจะเทรดเพชรให้ตั้งเป็น true ถ้าไม่คือ false (ตั้ง false หากเซิฟล่ม)
 }
