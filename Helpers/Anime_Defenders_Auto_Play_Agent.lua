e1442bd101416f06de592ba10a274a7ebfe07744
RamasaSakura <88618242+RamasaSakura@users.noreply.github.com>
2024-08-27 01:36:10 +0700

    Update Anime_Defenders_Auto_Play_Agent.lua

--- Helpers/Anime_Defenders_Auto_Play_Agent.lua
+++ Helpers/Anime_Defenders_Auto_Play_Agent.lua
@@ -182,7 +182,7 @@ local Selected_Folder = Paths_Folder:FindFirstChild(tostring(Selected_Path)) :: 
 
 
 local Total_Nodes = #Selected_Folder:GetChildren()
-local Starting_Node = Selected_Folder:FindFirstChild(tostring(Total_Nodes - math.round(((Config["Node Distance From Spawner"] or 0)+(Player_Index*3)))))
+local Starting_Node = Selected_Folder:FindFirstChild(tostring(Total_Nodes - math.round(((Config["Node Distance From Spawner"] or 0)+(Player_Index*4)))))
 
 local Current_Tracking_Node = Starting_Node :: BasePart
 
@@ -1074,7 +1074,7 @@ local function Initialize_Available_Unit()
 		plr.CharacterAdded:Wait()
 	end
 
-	Starting_Node = Selected_Folder:FindFirstChild(tostring(Total_Nodes - math.round(((Config["Node Distance From Spawner"] or 0)+(Player_Index*3)))))
+	Starting_Node = Selected_Folder:FindFirstChild(tostring(Total_Nodes - math.round(((Config["Node Distance From Spawner"] or 0)+(Player_Index*4)))))
 	Current_Tracking_Node = Starting_Node
 	
 	for _,v in Connections do
