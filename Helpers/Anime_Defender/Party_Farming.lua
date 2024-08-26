a381eb99bb13bcd36cf33db5fd507b9fa1b93370
RamasaSakura <88618242+RamasaSakura@users.noreply.github.com>
2024-08-26 17:07:22 +0700

    Update Party_Farming.lua

--- Helpers/Anime_Defender/Party_Farming.lua
+++ Helpers/Anime_Defender/Party_Farming.lua
@@ -1,6 +1,4 @@
 local Lobby_Id = 17017769292
-
-
 local ProximityPromptService = game:GetService("ProximityPromptService")
 local GuiService = game:GetService('GuiService')
 local RunService = game:GetService("RunService")
@@ -53,5 +51,40 @@ end
 if IsInLobbyGame() then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/RamasaSakura/Anime_Defender_Helper/main/Extra_Feature/PathFinder.lua'))()	
 else
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RamasaSakura/Anime_Defender_Helper/main/Helpers/Anime_Defenders_Auto_Play_Agent.lua'))()
 
+	if not game:IsLoaded() then
+	game.Loaded:Wait()
+	end
+
+local plr = game:GetService("Players").LocalPlayer
+local plr = game:GetService("Players").LocalPlayer
+
+local function MakeItClear(v: Instance?)
+	if v:IsA("BasePart") or v:IsA("Decal") or v:IsA("Texture") then
+		v.Transparency = 1
+		
+	elseif v:IsA("Beam") or v:IsA("ParticleEmitter") or v:IsA('BillboardGui') then
+		v.Enabled = false
+
+	end
+end
+
+
+for _,v in game:GetDescendants() do
+	MakeItClear(v)
+end
+
+
+game.DescendantAdded:Connect(MakeItClear)
+
+if not plr.Character then
+	plr.CharacterAdded:Wait()
+end
+
+if not plr:HasAppearanceLoaded() then
+	plr.CharacterAppearanceLoaded:Wait()
+end
+
+plr.Character:WaitForChild("Humanoid")
 end
